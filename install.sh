c0d7983b6083046d1fa34a22c728e206018e35d0
gustavo111336 <silvacostagustavo000@gmail.com>
2026-04-29 21:31:20 -0300

    Refactor install script for Dandelion repository
    Updated package management commands and improved installation process.

--- install.sh
+++ install.sh
@@ -1,5 +1,12 @@
-curl https://github.com/gustavo111336/meu-repo-dandelion/raw/refs/heads/main/dandelion-repo.deb
-apt update && apt upgrade 
-apt install ./dandelion-repo.deb
+pkg update -y && pkg upgrade -y
+
+pkg install curl -y
+
+curl -L -o dandelion-repo.deb https://github.com/gustavo111336/meu-repo-dandelion/raw/refs/heads/main/dandelion-repo.deb
+
+apt install ./dandelion-repo.deb -y
+
 apt update
-echo "new packages one dandelion-pkg dandelion-repo"
+
+echo "✔ Repo Dandelion instalado!"
+echo "Pacotes disponíveis: dandelion-pkg, dandelion-repo"
