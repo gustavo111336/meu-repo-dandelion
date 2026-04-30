e2938da040ef9e25c2c84a207619cd43f10af29c
gustavo111336 <silvacostagustavo000@gmail.com>
2026-04-29 21:32:29 -0300

    Add error handling for downloading dandelion-repo.deb

--- install.sh
+++ install.sh
@@ -3,6 +3,10 @@ pkg update -y && pkg upgrade -y
 pkg install curl -y
 
 curl -L -o dandelion-repo.deb https://github.com/gustavo111336/meu-repo-dandelion/raw/refs/heads/main/dandelion-repo.deb
+if [ ! -f dandelion-repo.deb ]; then
+  echo "Erro ao baixar o arquivo"
+  exit 1
+fi
 
 apt install ./dandelion-repo.deb -y
 
@@ -10,3 +14,4 @@ apt update
 
 echo "✔ Repo Dandelion instalado!"
 echo "Pacotes disponíveis: dandelion-pkg, dandelion-repo"
+rm dandelion-repo.deb
